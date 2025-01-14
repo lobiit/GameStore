f4d83ec615c3574b34d1f51e3f1a0de51609a04f
John Lobi Tumbo <john.lobi.tumbo@gmail.com>
2025-01-14 13:44:11 +0300

    generating the database.

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
-using API.DTOs;
+using API.Data;
 using API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("GameStore");
+
+builder.Services.AddSqlite<GameStoreContext>(connectionString);
+
 var app = builder.Build();
 
 app.MapGameEndpoints();
+
+app.MigrateDatabase();
 app.Run();
