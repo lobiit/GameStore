39ab32b921870342e4ad659c0b255ed2e0481454
John Lobi Tumbo <john.lobi.tumbo@gmail.com>
2025-01-15 13:14:37 +0300

    seed data.

--- API/Data/GameStoreContext.cs
+++ API/Data/GameStoreContext.cs
@@ -3,8 +3,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class GameStoreContext(DbContextOptions<GameStoreContext> options): DbContext(options)
+public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
 {
     public DbSet<Game> Games => Set<Game>();
     public DbSet<Genre> Genres => Set<Genre>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new { Id = 1, Name = "Sports" },
+            new { Id = 2, Name = "Fighting" },
+            new { Id = 3, Name = "Racing" },
+            new { Id = 4, Name = "Kids and Family" },
+            new { Id = 5, Name = "Sports" }
+        );
+    }
 }
